a3be2563f42125db571215568d14102bcb3b9589
steve <stevens714@yahoo.com>
2021-02-16 12:57:14 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,9 @@
+class StaticController < ApplicationController
+    def application
+        render "static/butts"
+    end
+
+    def hello_world
+        render "static/butts"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'application', to: 'static#application'
+    get 'hello_world', to: 'static#hello_world'
 end
+  
